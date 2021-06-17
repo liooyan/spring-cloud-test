7932f809bbf8c8da9bf1030ec6d2bc79b43139cc
liy <1064012668@qq.com>
2021-06-17 11:19:10 +0800

    fegin

---
+++ serviceA/src/main/java/com/serviceA/TestAutoConfiguration.java
@@ -0,0 +1,16 @@
+package com.serviceA;
+
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author top.lioyan
+ * @version 1.0
+ * @date 2021/6/9 11:27 上午
+ */
+public class TestAutoConfiguration {
+
+    @Bean
+    public static String cw() {
+        return null;
+    }
+}
---
+++ serviceA/src/main/java/com/stu/NacosDiscoveryStart.java
@@ -0,0 +1,16 @@
+package com.stu;
+
+import com.alibaba.cloud.nacos.registry.NacosAutoServiceRegistration;
+
+/**
+ * 注册中心的启动
+ * {@linkplain NacosAutoServiceRegistration}
+ *
+ *
+ *
+ * @author top.lioyan
+ * @version 1.0
+ * @date 2021/6/9 9:04 下午
+ */
+public class NacosDiscoveryStart {
+}
